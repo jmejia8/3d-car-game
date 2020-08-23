a13b66bb503194d3bac85cb4fe7d20d8688e1bfe
Jesus Mejia <jesusmejded@gmail.com>
2020-08-23 03:06:56 -0500

    update tileset

--- Drivers/IA.gd
+++ Drivers/IA.gd
@@ -4,23 +4,42 @@ var vehicle
 var left_sensor
 var frontal_sensor
 var frontal_small_sensor
+var left_small_sensor
+var right_small_sensor
 var right_sensor
 var steer_target_last = 1
 var target
+var target_idx
+var targets
 var stop = false
 
 
 func _ready():
+	steer_target_last = 1 if rand_range(0,1) > 0.5 else -1
 	vehicle = get_node("vehicle")
+
 	left_sensor = vehicle.get_node("FrontalSensors/Left")
 	frontal_sensor = vehicle.get_node("FrontalSensors/Front")
-	frontal_small_sensor = vehicle.get_node("FrontalSensors/Front")
 	right_sensor = vehicle.get_node("FrontalSensors/Right")
+
+
+	left_small_sensor = vehicle.get_node("FrontalSensors/FrontSmallLeft")
+	frontal_small_sensor = vehicle.get_node("FrontalSensors/FrontSmall")
+	right_small_sensor = vehicle.get_node("FrontalSensors/FrontSmallRight")
 	
 	
-	target = get_parent().get_node("Target")
+	
+	#target = get_parent().get_node("Targets/Target1")
+	targets = get_parent().get_node("Targets").get_children()
+	target_idx = 0
+	target = targets[target_idx]
+	#target = get_parent().get_node("Me/vehicle")
+	
 	if not target:
 		print("No target")
+	else:
+		for t in targets:
+			print(t.name)
 
 func look_for_target():
 	if not target:
@@ -32,60 +51,79 @@ func look_for_target():
 	#print(d)
 	return d
 
-func _physics_process(delta):
+func naive_control():
 	if stop:
 		vehicle.apply_brake()
 		return
+		
 	var steer_target = 0
-	var flag = true
+	var forward = 1
+	var backward = 0
+	var turn_left = 0
+	var turn_right = 0
+	var brake = 0
 	
 
 	if (frontal_small_sensor).is_colliding() or (left_sensor.is_colliding() and  right_sensor.is_colliding()):	
-		vehicle.reverse()
+		backward = 1
+		forward  = 0
 		if frontal_sensor.is_colliding(): #and !get_node("Sensors/Back").is_colliding():
-			vehicle.turn_steering(-steer_target_last, delta)
-		return
-	elif (frontal_small_sensor).is_colliding() && left_sensor.is_colliding():
-		vehicle.reverse()
-		vehicle.turn_steering(1, delta)
-		return
-	elif (frontal_small_sensor).is_colliding() && right_sensor.is_colliding():
-		vehicle.reverse()
-		vehicle.turn_steering(-1, delta)
-		return
+			turn_left =  1 if steer_target_last < 0 else 0
+			turn_right = 1 if steer_target_last > 0 else 0
+	elif (frontal_small_sensor).is_colliding() :# && left_sensor.is_colliding():
+		backward = 1
 	elif frontal_sensor.is_colliding() :
-		#brake_()
-		vehicle.brake = 0.0
-		vehicle.engine_force = 0
-		flag = false
-		vehicle.turn_steering(-steer_target_last, delta)
-		
-		
+		forward = 0
 
+	if backward > 0:
+		forward = 0
 	
 
 	if left_sensor.is_colliding():
-		steer_target = vehicle.RIGHT
-		
+		turn_right = 1
 	
 	if right_sensor.is_colliding() :
-		steer_target = vehicle.LEFT
-	
-	if steer_target == 0:
-		steer_target = look_for_target()
+		turn_left = 1
+
+	steer_target = turn_left - turn_right
+
+	if steer_target == 0 and forward > 0:
+		var t = look_for_target()
+		steer_target = t
 	else :
-		steer_target_last = steer_target
+		steer_target_last = steer_target if steer_target != 0 else steer_target_last
 
-	if flag :
-		vehicle.accelerate()
-		
+	return [forward, backward, steer_target, brake]
+
+
+func _physics_process(delta):
+	# [forward, backward, turn_left, turn_right, brake]
+	#var input = [
+	#	left_sensor.is_colliding(),
+	#	frontal_sensor.is_colliding(),
+	#	right_sensor.is_colliding(),
+	#	# small
+	#	left_small_sensor.is_colliding(),
+	#	frontal_small_sensor.is_colliding(),
+	#	right_small_sensor.is_colliding(),
+	#]
 	
-	vehicle.turn_steering(steer_target, delta)
-		
-
+	#print(input[0])
 	
 
 
-func _on_Area_body_entered(body):
-	print("Hola papu") # Replace with function body.
-	stop = body.is_in_group("IA_driver")
+	
+	var directions = naive_control()
+	vehicle.go_there(directions, delta)
+	
+
+
+
+func _on_Area_area_entered(area):
+	print("aaa = ", area.get_parent().name)
+	if area.is_in_group("target") &&  area.get_parent().name == "Target" + str(target_idx+1):
+		target_idx += 1
+		target = targets[target_idx]
+	print(target_idx)
+	
+	
--- scripts/vehicle.gd
+++ scripts/vehicle.gd
@@ -1,10 +1,11 @@
 extends VehicleBody
 
 const STEER_SPEED = 1
+const STEER_SPEED_TO_0 = 1.2
 const STEER_LIMIT = 0.5
 const SCALE_CAR_SPEED = 0.3
-const LEFT = -1
-const RIGHT = 1
+const LEFT = 1
+const RIGHT = -1
 
 
 var steer_target = 0
@@ -13,7 +14,7 @@ export var engine_force_value = 200
 export var reverse_engine_force_value = 100
 export var max_speed = 110
 
-var gear_shifts = [ 5, 2.5, 1, 0.5 ]
+var gear_shifts = [ 2.5, 1, 0.5, 0.2 ]
 var gear = 1
 # Car Actions
 
@@ -54,13 +55,35 @@ func reverse():
 	var fwd_mps = transform.basis.xform_inv(linear_velocity).z
 	if (fwd_mps <= 1):
 		brake = 0.0
-		engine_force = -reverse_engine_force_value*gear_shifts[1] if get_speed() < 15 else 0
+		engine_force = -reverse_engine_force_value*gear_shifts[0] if get_speed() < 15 else 0
 	else:
 		apply_brake()
 
 func turn_steering(direction, delta):
+	if direction == 0:
+		steering = move_toward(steering, direction, STEER_SPEED_TO_0 * delta )
+		return
+	
 	# hard to turn direction in high speed
 	var r = max(0.1, 1 - min(1, get_speed() / max_speed))
 	steer_target = direction*STEER_LIMIT
 	steering = move_toward(steering, steer_target, STEER_SPEED * delta * r)
+
+func go_there(directions, delta):
+	# directions = [forward, backward, steer_target, brake]
+	if directions[0] == 1:
+		accelerate()
+	if directions[1] == 1:
+		reverse()
+	if directions[3] == 1:
+		apply_brake()
+
+	turn_steering(directions[2], delta)
+
+
+
+
+
+
+
 
